53626995e0bc57feb2b3bf14f8f616e94b93ae85
Kalter <kalterxad34@gmail.com>
2017-11-21 19:28:27 +0300

    interceptors are added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,27 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import 'rxjs/add/operator/map';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {
+  constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.http.put('https://recipe-book-ffd00.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // const token = this.authService.getToken();
+    // const header = new HttpHeaders().set('Authorization', 'Bearer 123');
+    /*    return this.httpClient.put('https://recipe-book-ffd00.firebaseio.com/recipes.json',
+          this.recipeService.getRecipes(),
+          {
+            observe: 'body',
+            params: new HttpParams().set('auth', token)
+            // headers: header
+          });*/
+    const req = new HttpRequest(
+      'put',
+      'https://recipe-book-ffd00.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(), {
+        reportProgress: true,
+        // params: new HttpParams().set('auth', token)
+      });
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-    return this.http.get('https://recipe-book-ffd00.firebaseio.com/recipes.json?auth=' + token)
+    // const token = this.authService.getToken();
+    this.httpClient.get<Recipe[]>('https://recipe-book-ffd00.firebaseio.com/recipes.json')
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe.ingredients) {
               recipe.ingredients = [];
